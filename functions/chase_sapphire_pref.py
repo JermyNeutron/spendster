8108462dabb874179cff5d9d289af7db3e41b22e
Jeremy Desktop <jeremycrisostomo@gmail.com>
2024-06-09 03:42:08 -0700

    updated chase_sapphire_pref.py to successfully scrape 1-2 transaction pages

--- functions/chase_sapphire_pref.py
+++ functions/chase_sapphire_pref.py
@@ -6,6 +6,8 @@ import sys
 
 sys.path.append('.')
 
+import pyperclip
+
 from functions.inst_pars import extraction_func
 
 
@@ -87,70 +89,137 @@ def unpack_dict(stmt_essential_dic: dict):
 
 ''' First page scraping '''
 
-
 # Find starting index for transaction dates.
 def find_starting_dates(test):
+
+
+    # Count preceding dates to determine text gap to merchants
+    def count_backward(retro_counter):
+        line_skip = 0
+        skip_array = []
+        retro_counter -= 2
+        while retro_counter < len(extracted_text):
+            for line_number, line in enumerate(extracted_text, start=1):
+                if line_number == retro_counter:
+                    if date_pattern.match(line.strip()):
+                        line_skip += 2
+                        skip_array.append(line.strip())
+                        retro_counter -= 2
+                    else:
+                        if test:
+                            print(f"TEST: skipping {{{int(line_skip/2)}}} lines:")
+                            print(*(i for i in skip_array), sep='\n')
+                        return line_skip
+
+
+    def count_forward(counter):
+        while counter < len(extracted_text):
+            for line_number, line in enumerate(extracted_text, start=1):
+                if line_number == counter:
+                    if date_pattern.match(line.strip()):
+                        dates.append(line.strip())
+                        counter += 2
+                    else:
+                        if test:
+                            print(f"TEST: merchants start at {counter}")
+                            print(f"TEST: first page's dates: {dates}")
+                            print(f"TEST: array lenth: {len(dates)}")
+                            print(f"TEST: {find_starting_dates}: completed.")
+                        return dates, counter
+                    
+
     path = 'temp/temp_scrape.txt' if not test else 'temp/test_temp_scrape.txt'
-    counter = None
+    retro_counter = 0
+    counter = 0
     phrase = 'PURCHASE'
     with open(path, 'r') as file:
         extracted_text = file.readlines()
     for line_number, line in enumerate(extracted_text, start=1):
         if phrase in line:
+            retro_counter = line_number # counter for count_backwards()
             counter = line_number + 2 # add 2 to counter to start on dates
             if test:
                 print(f"\nTEST: dates start at: {counter}")
             break
     dates = []
     date_pattern = re.compile(r'^\d{2}/\d{2}$')
-    while counter < len(extracted_text):
-        for line_number, line in enumerate(extracted_text, start=1):
-            if line_number == counter:
-                if date_pattern.match(line.strip()):
-                    dates.append(line.strip())
-                    counter += 2
-                else:
-                    if test:
-                        print(f"\nTEST: merchants start at {counter}")
-                        print(f"TEST: first page's dates: {dates}")
-                        print(f"TEST: array lenth: {len(dates)}")
-                        print(f"TEST: {find_starting_dates}: completed.")
-                    return dates, counter
-    return dates, counter
+    line_skip = count_backward(retro_counter)
+    dates, counter = count_forward(counter)
+    return dates, counter, line_skip
 
 
 # Collect first page's merchants.
-def find_starting_merchants(test, merchant_counter):
+def find_starting_merchants(test, merchant_counter, line_skip):
+
+
+    # Redudant counter for '$ Amount'
+    def find_price_counter():
+        counter: int = 0
+        ending_phrase_1 = "$ Amount"
+        for line_number, line in enumerate(extracted_text, start=1):
+            if line.strip() == ending_phrase_1:
+                counter = line_number
+                return counter
+
+
+    if line_skip is None:
+        line_skip = 0
+    if test:
+        print(find_starting_merchants)
+        print(f"\nTEST: starting variable {{test}}: {test}")
+        print(f"TEST: starting variable {{merchant_counter}}: {merchant_counter}")
+        print(f"TEST: starting variable {{line_skip}}: {line_skip}")
     path = 'temp/temp_scrape.txt' if not test else 'temp/test_temp_scrape.txt'
     with open(path, 'r') as file:
         extracted_text = file.readlines()
     counter = None
-    ending_phrase_1 = '$ Amount'
+    reject_patterns = [r'^\d{6}',]
+    reject_counter = 0
+    ending_phrases = ['INTEREST CHARGED', 'FEES CHARGED', '$ Amount']
+    ending_flag = ""
     merchants = []
+    merchant_counter += line_skip
     while merchant_counter < len(extracted_text):
         for line_number, line in enumerate(extracted_text, start=1):
             if line_number == merchant_counter:
-                if ending_phrase_1 != line.strip():
-                    merchants.append(line.strip())
-                    merchant_counter += 2
+                if not any(re.match(pattern, line.strip()) for pattern in reject_patterns):
+                    if not any(line.strip() == phrase for phrase in ending_phrases):
+                        merchants.append(line.strip())
+                        merchant_counter += 2
+                    else:
+                        ending_flag = line.strip()
+                        counter = find_price_counter()
+                        if test:
+                            print(f"TEST: first page merchants: {merchants}")
+                            print(f"TEST: array length: {len(merchants)}")
+                            print(f"TEST: ending phrase caught: \"{ending_flag}\"")
+                            print(f"TEST: price starts at {counter}")
+                            print(f"TEST: {find_starting_merchants}: completed.")
+                        return merchants, counter, reject_counter
                 else:
-                    counter = line_number + 4
-                    if test:
-                        print(f"\nTEST: price starts at {counter}")
-                        print(f"TEST: first page merchants: {merchants}")
-                        print(f"TEST: array length: {len(merchants)}")
-                        print(f"TEST: {find_starting_merchants}: completed.")
-                    return merchants, counter
-    return merchants, counter
+                    merchant_counter += 2
+                    reject_counter += 1
+    return merchants, counter, reject_counter
 
 
 # Collect first page's amounts.
-def find_starting_amounts(test, price_counter):
+def find_starting_amounts(test, price_counter, line_skip, reject_counter):
+    if line_skip is None:
+        line_skip = 0
+    if test:
+        print(f"\nTEST: starting variable {{test}}: {test}")
+        print(f"TEST: starting variable {{price_counter}}: {price_counter}")
+        print(f"TEST: starting variable {{line_skip}}: {line_skip}")
+        print(f"TEST: starting variable {{reject_counter}}: {reject_counter}")
     path = 'temp/temp_scrape.txt' if not test else 'temp/test_temp_scrape.txt'
     with open(path, 'r') as file:
         extracted_text = file.readlines()
     counter = None
     price_amounts = []
+    differential = 0
+    if line_skip == 0:
+        differential = 2
+    price_counter = price_counter + 2 + line_skip + differential
     while price_counter < len(extracted_text):
         for line_number, line in enumerate(extracted_text, start=1):
             if line_number == price_counter:
@@ -159,10 +228,13 @@ def find_starting_amounts(test, price_counter):
                     price_counter += 2
                 except ValueError:
                     counter = line_number - 2
+                    if reject_counter != 0:
+                        del price_amounts[-reject_counter:]
                     if test:
-                        print(f"\nTEST: price ends at {counter}")
+                        print(f"TEST: reject_counter end: {reject_counter}")
                         print(f"TEST: first page amounts: {price_amounts}")
                         print(f"TEST: array length: {len(price_amounts)}")
+                        print(f"TEST: price ends at {counter}")
                         print(f"TEST: {find_starting_amounts}: completed.")
                     return price_amounts, counter
     return price_amounts, counter
@@ -184,12 +256,12 @@ def is_sp(test):
     if len(occurrences) >= 2:
         if test:
            print('\nTEST: There are multiple pages of transactions')
-           print(f"Occurrences: {occurrences}")
+           print(f"TEST: Occurrences: {occurrences}")
         return True
     else:
         if test:
            print('\nTEST: There is only one page of transactions')
-           print(f"Occurrences: {occurrences}")
+           print(f"TEST: Occurrences: {occurrences}")
         return False
 
 
@@ -276,15 +348,20 @@ def find_addl_amounts1(test, limit):
 # Pack and write organized data into csv.
 def create_csv(test, export_text):
     path = 'temp/temp.csv' if not test else 'temp/test_temp.csv'
-    with open(path, mode='w', newline='') as file:
+    with open(path, mode='w', newline='') as file: # writes CSV
         writer = csv.writer(file)
         writer.writerows(export_text)
-    if test:
+    with open(path, 'r', newline='') as file:
+        csv_data = list(csv.reader(file))
+        formatted_data = '\n'.join('\t'.join(row) for row in csv_data)
+        pyperclip.copy(formatted_data)
+        print("CSV content has been copied to clipboard. You can now paste it using CTRL+V.")
+    if test: # converts type: list into string to write into .txt
         csv_view = 'temp/test_csv_view.txt'
         with open(csv_view, 'w') as file:
             for item in export_text:
                 file.write(f"{str(item)}\n")
-        print('csv view created...')
+        print('TEST: csv view created...')
 
 
 # Main function of script.
@@ -302,9 +379,9 @@ def main(test, extracted_text, stmt_essential_keys=stmt_essential_keys):
     # Pack export_text to return.
     export_text.extend(unpack_dict(stmt_essential_dict))
     stmt_transactions = [('Dates', 'Merchants', 'Amount')]
-    fp_dates, fp_mercounter = find_starting_dates(test)
-    fp_merchants, fp_pricounter = find_starting_merchants(test, fp_mercounter)
-    fp_prices, fp_endcounter = find_starting_amounts(test, fp_pricounter) # NORMAL: fp_endcounter not referenced anywhere
+    fp_dates, fp_mercounter, fp_skipnum = find_starting_dates(test)
+    fp_merchants, fp_pricounter, fp_rejectcounter = find_starting_merchants(test, fp_mercounter, fp_skipnum)
+    fp_prices, fp_endcounter = find_starting_amounts(test, fp_pricounter, fp_skipnum, fp_rejectcounter) # NORMAL: fp_endcounter not referenced anywhere
     stmt_transactions.extend(zip(fp_dates, fp_merchants, fp_prices))
     if is_sp(test):
         sp_dates, sp_mercounter, sp_limit = find_addl_dates1(test)
@@ -321,7 +398,15 @@ def main(test, extracted_text, stmt_essential_keys=stmt_essential_keys):
 
 if __name__ == '__main__':
     test = True
-    path = 'rep_statements/20240111-statements-1149-.pdf'
+    option_1 = 'rep_statements/20240111-statements-1149-.pdf'
+    option_2 = 'rep_statements/20240511-statements-1149-.pdf'
+    choice = input('Choose path: ')
+    if choice == '1':
+        path = option_1
+    elif choice == '2':
+        path = option_2
+    else:
+        path = option_1
     extracted_text = extraction_func(path)
     main(test, extracted_text)
     # create_csv(test, export_text)
