3f463c1be5518a76d55c94f24f73f7e27fc529ac
Jeremy Desktop <jeremycrisostomo@gmail.com>
2024-06-15 16:17:56 -0700

    organized institution as dict and updated naming conventions

--- functions/inst_pars.py
+++ functions/inst_pars.py
@@ -4,10 +4,12 @@ from pdfminer.high_level import extract_text as pdfmextract_text
 
 '''Supported Institutions and Statements
 - Chase Sapphire Preferred
+- Chase Debit (WIP)
 '''
 
-list_institutions = ['Chase Mobile']
-ident_Chase = ['Chase Mobile', 'Sapphire Preferred', 'Sapphire Reserved', 'Chase']
+list_institutions = {
+    'Chase': ['Sapphire Preferred', 'Chase debit',]
+}
 
 
 # Extract text from the provided path.
@@ -24,40 +26,42 @@ def extraction_writing(test, text):
 
 
 # Parse text to identify the institution.
-def ident_inst(extracted_text):
-    for inst in list_institutions:
-        if inst in extracted_text:
-            return inst
+def ident_inst(test, extracted_text):
+    for inst, keywords in list_institutions.items():
+        for keyword in keywords:
+            if keyword in extracted_text:
+                if test:
+                    print(f"TESt: {{inst}}: {inst}")
+                    print(f"TEST: {{keyword}}: {keyword}")
+                return inst, keyword
 
 
 # Main function of script.
 def main(test, path):
     extracted_text = extraction_func(path)
-    institution = ident_inst(extracted_text)
-    if institution in ident_Chase:
-        if test:
-            print(f'\nTEST: {ident_inst}: INSTITUTION FOUND: {institution}')
-        inst_return =  "Chase"
-        extraction_writing(test, extracted_text)
-        return inst_return
-    else: # change to elif
-        return None
+    inst_select, inst_doc = ident_inst(test, extracted_text)
+    if test:
+        if inst_select == 'Chase':
+            print(f'TEST: {ident_inst}: INSTITUTION FOUND: Chase {inst_doc}')
+        else: # change to elif
+            return None
+    extraction_writing(test, extracted_text)
+    return inst_select, inst_doc
 
 
 if __name__ == '__main__':
     test = True
     path_1 = 'rep_statements/20240111-statements-1149-.pdf'
     path_2 = 'rep_statements/20240511-statements-1149-.pdf'
+    path_3 = 'rep_statements/20240320-statements-9266-.pdf'
     test_choice = input('Choose path: ')
     try:
         if test_choice == '1':
-            institution = main(test, path_1)
+            institution, document = main(test, path_1)
         elif test_choice == '2':
-            institution = main(test, path_2)
-        if institution in ident_Chase:
-            print('This document belongs to Chase Bank')
-        else:
-            print('wtf is this?!')
+            institution, document = main(test, path_2)
+        elif test_choice == '3':
+            institution, document = main(test, path_3)
     except NameError as e:
         print('Invalid choice.')
         
