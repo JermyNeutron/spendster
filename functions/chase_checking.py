9896ca59f017dd4c3c49b380b815d8162bf14ae6
Jeremy Desktop <jeremycrisostomo@gmail.com>
2024-06-19 17:48:31 -0700

    Separated stmt_keys out of function, created internal transaction class, implemented early strip(), split(), and removed empty lines for extracted_text for easier iteration, working on transaction iterations.

--- functions/chase_checking.py
+++ functions/chase_checking.py
@@ -12,8 +12,23 @@ from functions.inst_pars import extraction_func
 from calendar_months import months_dict 
 
 
+# Dictionary keys to return.
+stmt_essential_keys = ['month', 'period', 'balance',]
+
+
+# Transaction class
+class Transaction:
+    def __init__(self, date, merchant, amount) -> None:
+        self.date = date
+        self.merchant = merchant
+        self.amount = amount
+
+    def __repr__(self) -> str:
+        return f"Transaction({{{self.date}}}={self.date}, {{{self.merchant}}}={self.merchant}, {{{self.amount}}}={self.amount})"
+
+
 def keyword_search(test, extracted_text, keyphrase):
-    lines = extracted_text.splitlines()
+    lines = extracted_text
     for i, line in enumerate(lines):
         if keyphrase in line:
             if i + 2 < len(lines):
@@ -39,29 +54,58 @@ def find_month(test, extracted_text):
 def find_ending_balance(test, extracted_text):
     keyphrase = "Ending Balance"
     occurences = []
-    # Find the line number for the keyphrase
-    for i, line in enumerate(extracted_text.split('\n'), start=1):
+    # Find line number for keyphrase
+    for i, line in enumerate(extracted_text, start=1): #
         if keyphrase == line.strip():
             if test:
-                print(f"TEST: {find_ending_balance}: {i}, {line}")
+                print(f"TEST: {find_ending_balance}: {i}, {line.strip()}")
             occurences.append(i)
     # Find next line after keyphrase to return statement ending balance
     counter = occurences.pop(0) + 2
-    for i, line in enumerate(extracted_text.split('\n'), start=1):
+    for i, line in enumerate(extracted_text, start=1): #
         if counter == i:
             print(f"TEST: returning balance: {line}")
-            return line
+            return line.strip()
 
 
+def find_starting_transactions(test, extracted_text):
+    keyphrase = "Beginning Balance"
+    end_phrase = "*end*transaction detail"
+    occurences = []
+    # find line number for keyphrase
+    for i, line in enumerate(extracted_text, start=1): #
+        if keyphrase == line.strip():
+            if test:
+                print(f"TEST: {find_starting_transactions}: {i}, {line.strip()}")
+            occurences.append(i)
+    counter = occurences.pop() + 4
+    for i, line in enumerate(extracted_text, start=1): #
+        if counter == i:
+            for i in range(counter, len(extracted_text), 3):
+                pass
+    # Date, Merchant, Transaction
+    # +4 to repeat
+    # *end*transaction detail stop
 
-def main(test, extracted_text):
+
+def find_adl_transactions(test, extracted_text):
+    # *start*transaction detail, if x2 = 2nd page exists
+    # *end*transaction detail stop
+    pass
+
+
+def main(test, extracted_text, stmt_essential_keys=stmt_essential_keys):
     # Collecting CSV headers
+    extracted_text = [item for item in extracted_text.split('\n') if item != ''] # Chase Checking text requires .split('\n)
+    with open('temp/test_temp_scrape.txt', 'w', encoding='utf-8', errors='replace') as file: # removable
+        for item in extracted_text:
+            file.write(f"{str(item)}\n")
     if test:
         print(F"TEST: statement CSV headers")
-    stmt_essential_keys = ['month', 'period', 'balance',]
     stmt_essential_dict = {key: None for key in stmt_essential_keys}
     stmt_essential_dict['month'], stmt_essential_dict['period'] = find_month(test, extracted_text)
     stmt_essential_dict['balance'] = find_ending_balance(test, extracted_text)
+    find_starting_transactions(test, extracted_text)
 
 
 if __name__ == '__main__':
